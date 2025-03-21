7906957fd3b165e38612524406f6a9723a4e30ac
icodex <j.doit926@gmail.com>
2025-03-21 12:24:15 -0400

    Deployed at Fri Mar 21 12:24:15 PM EDT 2025

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1742573978',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
